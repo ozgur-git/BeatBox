4b711deea74496315bcce9fb212bc87d9d6840c7
ozgur-sys <ozgursevik@gmail.com>
2020-12-24 08:48:50 +0300

    first

---
+++ app/src/main/java/com/example/beatbox/BeatBox.java
@@ -0,0 +1,18 @@
+package com.example.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+public class BeatBox {
+
+    private static final String TAG="BeatBox";
+
+    private static final String SOUNDS_FOLDER="sample_sounds";
+
+    private AssetManager mAssets;
+
+    public BeatBox(Context context) {
+
+        mAssets=context.getAssets();
+    }
+}
