81e6d86b38e9b82f6a229d1daf2fe8d04da40a42
ozgur-sys <ozgursevik@gmail.com>
2020-12-27 16:42:56 +0300

    first

--- app/src/test/java/com/example/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/example/beatbox/SoundViewModelTest.java
@@ -1,9 +1,12 @@
 package com.example.beatbox;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import javax.inject.Inject;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class SoundViewModelTest {
@@ -15,9 +18,14 @@ public class SoundViewModelTest {
     @Before
     public void setUp() throws Exception {
         mBeatBox=mock(BeatBox.class);
-        mSound=new Sound("assetPath");
+        mSound=new Sound("assetPath/fileName");
         mSubject=new SoundViewModel(mBeatBox);
         mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle(){
+            assertThat(mSubject.getTitle(),is(mSound.getName()));
     }
 
 
