5087ba05c9189eb899f859939c03bf35a3a3b892
ozgur-sys <ozgursevik@gmail.com>
2020-12-27 15:45:39 +0300

    first

---
+++ app/src/test/java/com/example/beatbox/BeatBoxTest.java
@@ -0,0 +1,12 @@
+package com.example.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BeatBoxTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
---
+++ app/src/test/java/com/example/beatbox/SoundViewModelTest.java
@@ -0,0 +1,11 @@
+package com.example.beatbox;
+
+import org.junit.Before;
+
+public class SoundViewModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
