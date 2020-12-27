1ae97a93202fdfe8da591a78717d2d069561f088
ozgur-sys <ozgursevik@gmail.com>
2020-12-27 16:05:47 +0300

    first

--- app/src/test/java/com/example/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/example/beatbox/SoundViewModelTest.java
@@ -2,10 +2,23 @@ package com.example.beatbox;
 
 import org.junit.Before;
 
+import javax.inject.Inject;
+
+import static org.mockito.Mockito.mock;
+
 public class SoundViewModelTest {
+
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
 
     @Before
     public void setUp() throws Exception {
-
+        mBeatBox=mock(BeatBox.class);
+        mSound=new Sound("assetPath");
+        mSubject=new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
     }
+
+
 }
